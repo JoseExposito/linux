d74e55e24b1df0e89fd40c09e220018b0da00af6
adamrk <ark.email@gmail.com>
2021-03-13 22:01:44 +0100

    Implement array and string parameters

--- rust/module.rs
+++ rust/module.rs
@@ -11,12 +11,34 @@
 
 use proc_macro::{token_stream, Delimiter, Group, TokenStream, TokenTree};
 
-fn expect_ident(it: &mut token_stream::IntoIter) -> String {
-    if let TokenTree::Ident(ident) = it.next().unwrap() {
-        ident.to_string()
+fn try_ident(it: &mut token_stream::IntoIter) -> Option<String> {
+    if let Some(TokenTree::Ident(ident)) = it.next() {
+        Some(ident.to_string())
     } else {
-        panic!("Expected Ident");
+        None
     }
+}
+
+fn try_literal(it: &mut token_stream::IntoIter) -> Option<String> {
+    if let Some(TokenTree::Literal(literal)) = it.next() {
+        Some(literal.to_string())
+    } else {
+        None
+    }
+}
+
+fn try_byte_string(it: &mut token_stream::IntoIter) -> Option<String> {
+    try_literal(it).and_then(|byte_string| {
+        if byte_string.starts_with("b\"") && byte_string.ends_with('\"') {
+            Some(byte_string[2..byte_string.len() - 1].to_string())
+        } else {
+            None
+        }
+    })
+}
+
+fn expect_ident(it: &mut token_stream::IntoIter) -> String {
+    try_ident(it).expect("Expected Ident")
 }
 
 fn expect_punct(it: &mut token_stream::IntoIter) -> char {
@@ -28,11 +50,7 @@ fn expect_punct(it: &mut token_stream::IntoIter) -> char {
 }
 
 fn expect_literal(it: &mut token_stream::IntoIter) -> String {
-    if let TokenTree::Literal(literal) = it.next().unwrap() {
-        literal.to_string()
-    } else {
-        panic!("Expected Literal");
-    }
+    try_literal(it).expect("Expected Literal")
 }
 
 fn expect_group(it: &mut token_stream::IntoIter) -> Group {
@@ -40,6 +58,39 @@ fn expect_group(it: &mut token_stream::IntoIter) -> Group {
         group
     } else {
         panic!("Expected Group");
+    }
+}
+
+fn expect_byte_string(it: &mut token_stream::IntoIter) -> String {
+    try_byte_string(it).expect("Expected byte string")
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum ParamType {
+    Ident(String),
+    Array { vals: String, max_length: usize },
+}
+
+fn expect_array_fields(it: &mut token_stream::IntoIter) -> ParamType {
+    assert_eq!(expect_punct(it), '<');
+    let vals = expect_ident(it);
+    assert_eq!(expect_punct(it), ',');
+    let max_length_str = expect_literal(it);
+    let max_length = max_length_str
+        .parse::<usize>()
+        .expect("Expected usize length");
+    assert_eq!(expect_punct(it), '>');
+    ParamType::Array { vals, max_length }
+}
+
+fn expect_type(it: &mut token_stream::IntoIter) -> ParamType {
+    if let TokenTree::Ident(ident) = it.next().unwrap() {
+        match ident.to_string().as_ref() {
+            "ArrayParam" => expect_array_fields(it),
+            _ => ParamType::Ident(ident.to_string()),
+        }
+    } else {
+        panic!("Expected Param Type")
     }
 }
 
@@ -74,12 +125,11 @@ fn get_group(it: &mut token_stream::IntoIter, expected_name: &str) -> Group {
 }
 
 fn get_byte_string(it: &mut token_stream::IntoIter, expected_name: &str) -> String {
-    let byte_string = get_literal(it, expected_name);
-
-    assert!(byte_string.starts_with("b\""));
-    assert!(byte_string.ends_with('\"'));
-
-    byte_string[2..byte_string.len() - 1].to_string()
+    assert_eq!(expect_ident(it), expected_name);
+    assert_eq!(expect_punct(it), ':');
+    let byte_string = expect_byte_string(it);
+    assert_eq!(expect_punct(it), ',');
+    byte_string
 }
 
 fn __build_modinfo_string_base(
@@ -177,6 +227,91 @@ fn permissions_are_readonly(perms: &str) -> bool {
     }
 }
 
+fn param_ops_path(param_type: &str) -> &'static str {
+    match param_type {
+        "bool" => "kernel::module_param::PARAM_OPS_BOOL",
+        "i8" => "kernel::module_param::PARAM_OPS_I8",
+        "u8" => "kernel::module_param::PARAM_OPS_U8",
+        "i16" => "kernel::module_param::PARAM_OPS_I16",
+        "u16" => "kernel::module_param::PARAM_OPS_U16",
+        "i32" => "kernel::module_param::PARAM_OPS_I32",
+        "u32" => "kernel::module_param::PARAM_OPS_U32",
+        "i64" => "kernel::module_param::PARAM_OPS_I64",
+        "u64" => "kernel::module_param::PARAM_OPS_U64",
+        "isize" => "kernel::module_param::PARAM_OPS_ISIZE",
+        "usize" => "kernel::module_param::PARAM_OPS_USIZE",
+        "str" => "kernel::module_param::PARAM_OPS_STR",
+        t => panic!("Unrecognized type {}", t),
+    }
+}
+
+fn try_simple_param_val(
+    param_type: &str,
+) -> Box<dyn Fn(&mut token_stream::IntoIter) -> Option<String>> {
+    match param_type {
+        "bool" => Box::new(|param_it| try_ident(param_it)),
+        "str" => Box::new(|param_it| {
+            try_byte_string(param_it)
+                .map(|s| format!("kernel::module_param::StringParam::Ref(b\"{}\")", s))
+        }),
+        _ => Box::new(|param_it| try_literal(param_it)),
+    }
+}
+
+fn get_default(param_type: &ParamType, param_it: &mut token_stream::IntoIter) -> String {
+    let try_param_val = match param_type {
+        ParamType::Ident(ref param_type)
+        | ParamType::Array {
+            vals: ref param_type,
+            max_length: _,
+        } => try_simple_param_val(param_type),
+    };
+    assert_eq!(expect_ident(param_it), "default");
+    assert_eq!(expect_punct(param_it), ':');
+    let default = match param_type {
+        ParamType::Ident(_) => try_param_val(param_it).expect("Expected default param value"),
+        ParamType::Array {
+            vals: _,
+            max_length: _,
+        } => {
+            let group = expect_group(param_it);
+            assert_eq!(group.delimiter(), Delimiter::Bracket);
+            let mut default_vals = Vec::new();
+            let mut it = group.stream().into_iter();
+
+            while let Some(default_val) = try_param_val(&mut it) {
+                default_vals.push(default_val);
+                match it.next() {
+                    Some(TokenTree::Punct(punct)) => assert_eq!(punct.as_char(), ','),
+                    None => break,
+                    _ => panic!("Expected ',' or end of array default values"),
+                }
+            }
+
+            let mut default_array = "kernel::module_param::ArrayParam::create(&[".to_string();
+            default_array.push_str(
+                &default_vals
+                    .iter()
+                    .map(|val| val.to_string())
+                    .collect::<Vec<String>>()
+                    .join(","),
+            );
+            default_array.push_str("])");
+            default_array
+        }
+    };
+    assert_eq!(expect_punct(param_it), ',');
+    default
+}
+
+fn generated_array_ops_name(vals: &str, max_length: usize) -> String {
+    format!(
+        "__generated_array_ops_{vals}_{max_length}",
+        vals = vals,
+        max_length = max_length
+    )
+}
+
 /// Declares a kernel module.
 ///
 /// The `type` argument should be a type which implements the [`KernelModule`]
@@ -242,6 +377,8 @@ fn permissions_are_readonly(perms: &str) -> bool {
 ///   - `usize`: No equivalent C param type.
 ///   - `str`: Corresponds to C `charp` param type. Reading returns a byte
 ///     slice.
+///   - `ArrayParam<T,N>`: Corresponds to C parameters created using
+///     `module_param_array`. An array of `T`'s of length at **most** `N`.
 ///
 /// `invbool` is unsupported: it was only ever used in a few modules.
 /// Consider using a `bool` and inverting the logic instead.
@@ -264,6 +401,8 @@ pub fn module(ts: TokenStream) -> TokenStream {
 
     let mut params_modinfo = String::new();
 
+    let mut array_types_to_generate = Vec::new();
+
     loop {
         let param_name = match it.next() {
             Some(TokenTree::Ident(ident)) => ident.to_string(),
@@ -272,38 +411,35 @@ pub fn module(ts: TokenStream) -> TokenStream {
         };
 
         assert_eq!(expect_punct(&mut it), ':');
-        let param_type = expect_ident(&mut it);
+        let param_type = expect_type(&mut it);
         let group = expect_group(&mut it);
         assert_eq!(expect_punct(&mut it), ',');
 
         assert_eq!(group.delimiter(), Delimiter::Brace);
 
         let mut param_it = group.stream().into_iter();
-        let param_default = match param_type.as_ref() {
-            "bool" => get_ident(&mut param_it, "default"),
-            "str" => get_byte_string(&mut param_it, "default"),
-            _ => get_literal(&mut param_it, "default"),
-        };
+        let param_default = get_default(&param_type, &mut param_it);
         let param_permissions = get_literal(&mut param_it, "permissions");
         let param_description = get_byte_string(&mut param_it, "description");
         expect_end(&mut param_it);
 
         // TODO: more primitive types
-        // TODO: other kinds: arrays, unsafes, etc.
-        let (param_kernel_type, ops) = match param_type.as_ref() {
-            "bool" => ("bool", "kernel::module_param::PARAM_OPS_BOOL"),
-            "i8" => ("i8", "kernel::module_param::PARAM_OPS_I8"),
-            "u8" => ("u8", "kernel::module_param::PARAM_OPS_U8"),
-            "i16" => ("i16", "kernel::module_param::PARAM_OPS_I16"),
-            "u16" => ("u16", "kernel::module_param::PARAM_OPS_U16"),
-            "i32" => ("i32", "kernel::module_param::PARAM_OPS_I32"),
-            "u32" => ("u32", "kernel::module_param::PARAM_OPS_U32"),
-            "i64" => ("i64", "kernel::module_param::PARAM_OPS_I64"),
-            "u64" => ("u64", "kernel::module_param::PARAM_OPS_U64"),
-            "isize" => ("isize", "kernel::module_param::PARAM_OPS_ISIZE"),
-            "usize" => ("usize", "kernel::module_param::PARAM_OPS_USIZE"),
-            "str" => ("charp", "kernel::bindings::param_ops_charp"),
-            t => panic!("Unrecognized type {}", t),
+        // TODO: other kinds: unsafes, etc.
+        let (param_kernel_type, ops): (String, _) = match param_type {
+            ParamType::Ident(ref param_type) => (
+                param_type.to_string(),
+                param_ops_path(&param_type).to_string(),
+            ),
+            ParamType::Array {
+                ref vals,
+                max_length,
+            } => {
+                array_types_to_generate.push((vals.clone(), max_length));
+                (
+                    format!("__rust_array_param_{}_{}", vals, max_length),
+                    generated_array_ops_name(vals, max_length),
+                )
+            }
         };
 
         params_modinfo.push_str(&build_modinfo_string_param(
@@ -318,68 +454,44 @@ pub fn module(ts: TokenStream) -> TokenStream {
             &param_name,
             &param_description,
         ));
-        let param_type_internal = match param_type.as_ref() {
-            "str" => "*mut kernel::c_types::c_char",
-            _ => &param_type,
+        let param_type_internal = match param_type {
+            ParamType::Ident(ref param_type) => match param_type.as_ref() {
+                "str" => "kernel::module_param::StringParam".to_string(),
+                other => other.to_string(),
+            },
+            ParamType::Array {
+                ref vals,
+                max_length,
+            } => format!(
+                "kernel::module_param::ArrayParam<{vals}, {max_length}>",
+                vals = vals,
+                max_length = max_length
+            ),
         };
-        let param_default = match param_type.as_ref() {
-            "str" => format!(
-                "b\"{}\0\" as *const _ as *mut kernel::c_types::c_char",
-                param_default
-            ),
-            _ => param_default,
-        };
-        let read_func = match (param_type.as_ref(), permissions_are_readonly(&param_permissions)) {
-            ("str", false) => format!(
+        let read_func = if permissions_are_readonly(&param_permissions) {
+            format!(
                 "
-                    fn read<'lck>(&self, lock: &'lck kernel::KParamGuard) -> &'lck [u8] {{
-                        // SAFETY: The pointer is provided either in `param_default` when building the module,
-                        // or by the kernel through `param_set_charp`. Both will be valid C strings.
-                        // Parameters are locked by `KParamGuard`.
-                        unsafe {{
-                            kernel::c_types::c_string_bytes(__{name}_{param_name}_value)
-                        }}
-                    }}
-                ",
-                name = name,
-                param_name = param_name,
-            ),
-            ("str", true) => format!(
-                "
-                    fn read(&self) -> &[u8] {{
-                        // SAFETY: The pointer is provided either in `param_default` when building the module,
-                        // or by the kernel through `param_set_charp`. Both will be valid C strings.
-                        // Parameters do not need to be locked because they are read only or sysfs is not enabled.
-                        unsafe {{
-                            kernel::c_types::c_string_bytes(__{name}_{param_name}_value)
-                        }}
-                    }}
-                ",
-                name = name,
-                param_name = param_name,
-            ),
-            (_, false) => format!(
-                "
-                    // SAFETY: Parameters are locked by `KParamGuard`.
-                    fn read<'lck>(&self, lock: &'lck kernel::KParamGuard) -> &'lck {param_type_internal} {{
-                        unsafe {{ &__{name}_{param_name}_value }}
+                    fn read(&self) -> &<{param_type_internal} as kernel::module_param::ModuleParam>::Value {{
+                        // SAFETY: Parameters do not need to be locked because they are read only or sysfs is not enabled.
+                        unsafe {{ <{param_type_internal} as kernel::module_param::ModuleParam>::value(&__{name}_{param_name}_value) }}
                     }}
                 ",
                 name = name,
                 param_name = param_name,
                 param_type_internal = param_type_internal,
-            ),
-            (_, true) => format!(
+            )
+        } else {
+            format!(
                 "
-                    // SAFETY: Parameters do not need to be locked because they are read only or sysfs is not enabled.
-                    fn read(&self) -> &{param_type_internal} {{
-                        unsafe {{ &__{name}_{param_name}_value }}
+                    fn read<'lck>(&self, lock: &'lck kernel::KParamGuard) -> &'lck <{param_type_internal} as kernel::module_param::ModuleParam>::Value {{
+                        // SAFETY: Parameters are locked by `KParamGuard`.
+                        unsafe {{ <{param_type_internal} as kernel::module_param::ModuleParam>::value(&__{name}_{param_name}_value) }}
                     }}
                 ",
                 name = name,
                 param_name = param_name,
                 param_type_internal = param_type_internal,
-            ),
+            )
         };
         let kparam = format!(
             "
@@ -446,6 +558,23 @@ pub fn module(ts: TokenStream) -> TokenStream {
                 kparam = kparam,
             )
         );
+    }
+
+    let mut generated_array_types = String::new();
+
+    for (vals, max_length) in array_types_to_generate {
+        let ops_name = generated_array_ops_name(&vals, max_length);
+        generated_array_types.push_str(&format!(
+            "
+                kernel::make_param_ops!(
+                    {ops_name},
+                    kernel::module_param::ArrayParam<{vals}, {{ {max_length} }}>
+                );
+            ",
+            ops_name = ops_name,
+            vals = vals,
+            max_length = max_length,
+        ));
     }
 
     let file = std::env::var("RUST_MODFILE").unwrap();
@@ -532,6 +661,8 @@ pub fn module(ts: TokenStream) -> TokenStream {
             {file}
 
             {params_modinfo}
+
+            {generated_array_types}
         ",
         type_ = type_,
         name = name,
@@ -540,6 +671,7 @@ pub fn module(ts: TokenStream) -> TokenStream {
         license = &build_modinfo_string(&name, "license", &license),
         file = &build_modinfo_string_only_builtin(&name, "file", &file),
         params_modinfo = params_modinfo,
+        generated_array_types = generated_array_types,
         initcall_section = ".initcall6.init"
     ).parse().unwrap()
 }
