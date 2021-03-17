b540c4536c65d7f92f19cc32af96bf3ba0ff7c0c
Wedson Almeida Filho <wedsonaf@google.com>
2021-03-17 16:09:00 +0000

    Simplify how clients define file operations.
    This was discussed on the following thread:
    https://lore.kernel.org/rust-for-linux/CANiq72kHf8m2BGcj+znWnoLzfy_1kP3BQuRAmGTZXnW3tdtnhA@mail.gmail.com/T/

--- drivers/char/rust_example.rs
+++ drivers/char/rust_example.rs
@@ -51,6 +51,8 @@ struct RustFile;
 impl FileOperations for RustFile {
     type Wrapper = Box<Self>;
 
+    kernel::declare_file_operations!();
+
     fn open() -> KernelResult<Self::Wrapper> {
         println!("rust file was opened!");
         Ok(Box::try_new(Self)?)
