fa6f1bac3c6a1c1d44decd621e5bd5f68787d395
Sven Van Asbroeck <thesven73@gmail.com>
2021-05-26 10:58:03 -0400

    bcm2835_rng_rust: implement `PlatformDriver` trait
    Implement this in the simplest possible way: by printing a
    `pr_info` log message on `probe()`/`remove()`.
    
    Signed-off-by: Sven Van Asbroeck <thesven73@gmail.com>

--- drivers/char/hw_random/bcm2835_rng_rust.rs
+++ drivers/char/hw_random/bcm2835_rng_rust.rs
@@ -8,6 +8,7 @@
 use alloc::boxed::Box;
 use core::pin::Pin;
 use kernel::of::OfMatchTable;
+use kernel::platdev::PlatformDriver;
 use kernel::prelude::*;
 use kernel::{c_str, platdev};
 
@@ -19,6 +20,20 @@ module! {
     license: b"GPL v2",
 }
 
+struct RngDriver;
+
+impl PlatformDriver for RngDriver {
+    fn probe(device_id: i32) -> Result {
+        pr_info!("probing discovered hwrng with id {}\n", device_id);
+        Ok(())
+    }
+
+    fn remove(device_id: i32) -> Result {
+        pr_info!("removing hwrng with id {}\n", device_id);
+        Ok(())
+    }
+}
+
 struct RngModule {
     _pdev: Pin<Box<platdev::Registration>>,
 }
@@ -27,7 +42,7 @@ impl KernelModule for RngModule {
     fn init() -> Result<Self> {
         let of_match_tbl = OfMatchTable::new(&c_str!("brcm,bcm2835-rng"))?;
 
-        let pdev = platdev::Registration::new_pinned(
+        let pdev = platdev::Registration::new_pinned::<RngDriver>(
             c_str!("bcm2835-rng-rust"),
             Some(of_match_tbl),
             &THIS_MODULE,
