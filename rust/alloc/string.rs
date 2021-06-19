af999c4383d829802bb5fc3ae30056b7c1ef545e
Miguel Ojeda <ojeda@kernel.org>
2021-06-19 23:31:08 +0200

    rust: alloc: fix unused import `from_fn` warning
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/alloc/string.rs
+++ rust/alloc/string.rs
@@ -49,8 +49,8 @@ use core::char::{decode_utf16, REPLACEMENT_CHARACTER};
 use core::fmt;
 use core::hash;
 #[cfg(not(no_global_oom_handling))]
-use core::iter::FromIterator;
-use core::iter::{from_fn, FusedIterator};
+use core::iter::{from_fn, FromIterator};
+use core::iter::FusedIterator;
 #[cfg(not(no_global_oom_handling))]
 use core::ops::Add;
 #[cfg(not(no_global_oom_handling))]
