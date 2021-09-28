d508f60a6fb44fcfdc5f6704358b0815d84c1d9e
Matthew Bakhtiari <dev@mtbk.me>
2021-09-28 11:48:09 +0200

    rust: port liballoc to release 1.55.0
    Added `#[cfg(not(no_global_oom_handling))]` to `Arc::try_pin` because
    `Pin` is only imported when `no_global_oom_handling` isn't selected
    
    Signed-off-by: Matthew Bakhtiari <dev@mtbk.me>

--- rust/alloc/borrow.rs
+++ rust/alloc/borrow.rs
@@ -179,6 +179,7 @@ where
 /// }
 /// ```
 #[stable(feature = "rust1", since = "1.0.0")]
+#[cfg_attr(not(test), rustc_diagnostic_item = "Cow")]
 pub enum Cow<'a, B: ?Sized + 'a>
 where
     B: ToOwned,
