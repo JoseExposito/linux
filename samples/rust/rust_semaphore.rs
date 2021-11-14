36a85e4f73d9e17fa61145fb035a9f302b591678
Wedson Almeida Filho <wedsonaf@google.com>
2021-11-14 17:40:01 +0000

    rust: remove all usages of `try_new_and_init` and `pin_init_and_share`.
    Signed-off-by: Wedson Almeida Filho <wedsonaf@google.com>

--- samples/rust/rust_semaphore.rs
+++ samples/rust/rust_semaphore.rs
@@ -25,7 +25,7 @@ use kernel::{
     miscdev::Registration,
     mutex_init,
     prelude::*,
-    sync::{CondVar, Mutex, Ref},
+    sync::{CondVar, Mutex, Ref, UniqueRef},
     user_ptr::{UserSlicePtrReader, UserSlicePtrWriter},
 };
 
@@ -113,32 +113,29 @@ impl KernelModule for RustSemaphore {
     fn init(name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
         pr_info!("Rust semaphore sample (init)\n");
 
-        let sema = Ref::try_new_and_init(
-            Semaphore {
-                // SAFETY: `condvar_init!` is called below.
-                changed: unsafe { CondVar::new() },
+        let mut sema = Pin::from(UniqueRef::try_new(Semaphore {
+            // SAFETY: `condvar_init!` is called below.
+            changed: unsafe { CondVar::new() },
 
-                // SAFETY: `mutex_init!` is called below.
-                inner: unsafe {
-                    Mutex::new(SemaphoreInner {
-                        count: 0,
-                        max_seen: 0,
-                    })
-                },
+            // SAFETY: `mutex_init!` is called below.
+            inner: unsafe {
+                Mutex::new(SemaphoreInner {
+                    count: 0,
+                    max_seen: 0,
+                })
             },
-            |mut sema| {
-                // SAFETY: `changed` is pinned when `sema` is.
-                let pinned = unsafe { sema.as_mut().map_unchecked_mut(|s| &mut s.changed) };
-                condvar_init!(pinned, "Semaphore::changed");
+        })?);
 
-                // SAFETY: `inner` is pinned when `sema` is.
-                let pinned = unsafe { sema.as_mut().map_unchecked_mut(|s| &mut s.inner) };
-                mutex_init!(pinned, "Semaphore::inner");
-            },
-        )?;
+        // SAFETY: `changed` is pinned when `sema` is.
+        let pinned = unsafe { sema.as_mut().map_unchecked_mut(|s| &mut s.changed) };
+        condvar_init!(pinned, "Semaphore::changed");
+
+        // SAFETY: `inner` is pinned when `sema` is.
+        let pinned = unsafe { sema.as_mut().map_unchecked_mut(|s| &mut s.inner) };
+        mutex_init!(pinned, "Semaphore::inner");
 
         Ok(Self {
-            _dev: Registration::new_pinned::<FileState>(name, None, sema)?,
+            _dev: Registration::new_pinned::<FileState>(name, None, sema.into())?,
         })
     }
 }
