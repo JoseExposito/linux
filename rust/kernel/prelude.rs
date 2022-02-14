2ef2e88a2f123689b5a0137bd5145ce6c09fd7fe
Wedson Almeida Filho <wedsonaf@google.com>
2022-02-14 16:49:39 +0000

    rust: add basic owned C strings
    For now they're only constructible from formatted arguments. They will
    be used in a subsequent patch to allow registration of miscdevs with
    names that are constructed at runtime.
    
    Signed-off-by: Wedson Almeida Filho <wedsonaf@google.com>

--- rust/kernel/prelude.rs
+++ rust/kernel/prelude.rs
@@ -20,7 +20,7 @@ pub use macros::module;
 pub use super::build_assert;
 
 pub use super::{
-    dbg, dev_alert, dev_crit, dev_dbg, dev_emerg, dev_err, dev_info, dev_notice, dev_warn,
+    dbg, dev_alert, dev_crit, dev_dbg, dev_emerg, dev_err, dev_info, dev_notice, dev_warn, fmt,
     pr_alert, pr_crit, pr_debug, pr_emerg, pr_err, pr_info, pr_notice, pr_warn,
 };
 
