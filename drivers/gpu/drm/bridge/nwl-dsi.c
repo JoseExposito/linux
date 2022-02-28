a7448b895a4451760aeca7a32ab69a1fcb692e4a
José Expósito <jose.exposito89@gmail.com>
2022-02-28 19:21:51 +0100

    drm/bridge: nwl-dsi: switch to devm_drm_of_get_bridge
    The function "drm_of_find_panel_or_bridge" has been deprecated in
    favor of "devm_drm_of_get_bridge".
    
    Switch to the new function and reduce boilerplate.
    
    Signed-off-by: José Expósito <jose.exposito89@gmail.com>

--- drivers/gpu/drm/bridge/nwl-dsi.c
+++ drivers/gpu/drm/bridge/nwl-dsi.c
@@ -909,19 +909,12 @@ static int nwl_dsi_bridge_attach(struct drm_bridge *bridge,
 {
 	struct nwl_dsi *dsi = bridge_to_dsi(bridge);
 	struct drm_bridge *panel_bridge;
-	struct drm_panel *panel;
-	int ret;
 
-	ret = drm_of_find_panel_or_bridge(dsi->dev->of_node, 1, 0, &panel,
-					  &panel_bridge);
-	if (ret)
-		return ret;
+	panel_bridge = devm_drm_of_get_bridge(dsi->dev, dsi->dev->of_node,
+					      1, 0);
+	if (IS_ERR(panel_bridge))
+		return PTR_ERR(panel_bridge);
 
-	if (panel) {
-		panel_bridge = drm_panel_bridge_add(panel);
-		if (IS_ERR(panel_bridge))
-			return PTR_ERR(panel_bridge);
-	}
 	dsi->panel_bridge = panel_bridge;
 
 	if (!dsi->panel_bridge)
