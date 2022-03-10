5ed068421137c20fec9ff0e76931da4b0f70e162
Miguel Ojeda <ojeda@kernel.org>
2022-03-10 14:40:22 +0100

    rust: target: move x86 redzone option to command-line
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- scripts/generate_rust_target.rs
+++ scripts/generate_rust_target.rs
@@ -216,7 +216,6 @@ fn main() {
             "data-layout",
             "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128",
         );
-        ts.push("disable-redzone", true);
         ts.push(
             "features",
             "-mmx,-sse,-sse2,-sse3,-ssse3,-sse4.1,-sse4.2,-3dnow,-3dnowa,-avx,-avx2,+soft-float",
