ea19cabecebe46391138a9bda4678306b89dfcab
Miguel Ojeda <ojeda@kernel.org>
2022-03-13 03:19:28 +0100

    rust: target: remove `dynamic-linking`
    We do not use `dylib`s, `cdylib`s nor cross-compiled proc macros.
    
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- scripts/generate_rust_target.rs
+++ scripts/generate_rust_target.rs
@@ -152,7 +152,6 @@ fn main() {
             "data-layout",
             "e-m:e-p:32:32-Fi8-i64:64-v128:64:128-a:0:32-n32-S64",
         );
-        ts.push("dynamic-linking", true);
         ts.push("crt-static-respected", true);
         ts.push("features", "+strict-align,+v6");
         ts.push("llvm-target", "arm-linux-gnueabi");
