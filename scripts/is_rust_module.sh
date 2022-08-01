13947745f6ecb676f6bf439939a01d270bc91938
Miguel Ojeda <ojeda@kernel.org>
2022-08-01 16:27:24 +0200

    scripts: is_rust_module: add comment and link about `.comment`
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- scripts/is_rust_module.sh
+++ scripts/is_rust_module.sh
@@ -10,4 +10,7 @@ set -e
 # Using the `16_` prefix ensures other symbols with the same substring
 # are not picked up (even if it would be unlikely). The last part is
 # used just in case LLVM decides to use the `.` suffix.
+#
+# In the future, checking for the `.comment` section may be another
+# option, see https://github.com/rust-lang/rust/pull/97550.
 ${NM} "$*" | grep -qE '^[0-9a-fA-F]+ r _R[^[:space:]]+16___IS_RUST_MODULE[^[:space:]]*$'
