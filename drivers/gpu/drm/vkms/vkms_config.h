0db108e945a137e76f4e25f3a03aeb796cda4c84
José Expósito <jose.exposito89@gmail.com>
2024-07-28 12:13:48 +0200

    drm/vkms: Allow to configure multiple CRTCs
    Add a list of CRTC configurations to vkms_config and add as many CRTCs
    as configured during output initialization.
    
    For backwards compatibility, create a single CRTC in the default
    configuration.
    
    Since writeback support, vblank and composer states are now per CRTC,
    extract all the fields to the vkms_crtc structure and allow each
    vkms_device to have a list of CRTCs.
    
    Signed-off-by: José Expósito <jose.exposito89@gmail.com>

--- drivers/gpu/drm/vkms/vkms_config.h
+++ drivers/gpu/drm/vkms/vkms_config.h
@@ -3,15 +3,22 @@
 #ifndef _VKMS_CONFIG_H_
 #define _VKMS_CONFIG_H_
 
+#include <linux/list.h>
 #include <linux/types.h>
 
 struct vkms_device;
 
+struct vkms_config_crtc {
+	struct list_head list;
+	unsigned int index;
+	bool writeback;
+};
+
 struct vkms_config {
 	char *dev_name;
-	bool writeback;
 	bool cursor;
 	bool overlay;
+	struct list_head crtcs;
 	/* only set when instantiated */
 	struct vkms_device *dev;
 };
@@ -25,5 +32,11 @@ void vkms_config_destroy(struct vkms_config *config);
 
 /* DebugFS */
 void vkms_config_debugfs_init(struct vkms_device *vkms_device);
+
+/* CRTCs */
+struct vkms_config_crtc *vkms_config_add_crtc(struct vkms_config *config,
+					      bool enable_writeback);
+void vkms_config_destroy_crtc(struct vkms_config *config,
+			      struct vkms_config_crtc *crtc_cfg);
 
 #endif /* _VKMS_CONFIG_H_ */
