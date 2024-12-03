3acfed9d415d107283f76141b0197637de6f0554
José Expósito <jose.exposito89@gmail.com>
2024-12-03 16:48:12 +0100

    drm/vkms: Allow to configure multiple CRTCs
    Add a list of CRTCs to vkms_config and helper functions to add and
    remove as many CRTCs as wanted.
    
    For backwards compatibility, add one CRTC to the default configuration.
    
    A future patch will allow to attach planes and CRTCs, but for the
    moment there are no changes in the way the output is configured.
    
    Signed-off-by: Louis Chauvet <louis.chauvet@bootlin.com>
    Signed-off-by: José Expósito <jose.exposito89@gmail.com>

--- drivers/gpu/drm/vkms/vkms_config.c
+++ drivers/gpu/drm/vkms/vkms_config.c
@@ -22,6 +22,7 @@ struct vkms_config *vkms_config_create(const char *dev_name)
 	}
 
 	INIT_LIST_HEAD(&config->planes);
+	INIT_LIST_HEAD(&config->crtcs);
 
 	return config;
 }
@@ -32,18 +33,22 @@ struct vkms_config *vkms_config_default_create(bool enable_cursor,
 {
 	struct vkms_config *config;
 	struct vkms_config_plane *plane_cfg;
+	struct vkms_config_crtc *crtc_cfg;
 	int n;
 
 	config = vkms_config_create(DEFAULT_DEVICE_NAME);
 	if (IS_ERR(config))
 		return config;
 
-	config->writeback = enable_writeback;
-
 	plane_cfg = vkms_config_add_plane(config);
 	if (IS_ERR(plane_cfg))
 		goto err_alloc;
 	plane_cfg->type = DRM_PLANE_TYPE_PRIMARY;
+
+	crtc_cfg = vkms_config_add_crtc(config);
+	if (IS_ERR(crtc_cfg))
+		goto err_alloc;
+	crtc_cfg->writeback = enable_writeback;
 
 	if (enable_overlay) {
 		for (n = 0; n < NUM_OVERLAY_PLANES; n++) {
@@ -71,9 +76,13 @@ err_alloc:
 void vkms_config_destroy(struct vkms_config *config)
 {
 	struct vkms_config_plane *plane_cfg, *plane_tmp;
+	struct vkms_config_crtc *crtc_cfg, *crtc_tmp;
 
 	list_for_each_entry_safe(plane_cfg, plane_tmp, &config->planes, link)
 		vkms_config_destroy_plane(plane_cfg);
+
+	list_for_each_entry_safe(crtc_cfg, crtc_tmp, &config->crtcs, link)
+		vkms_config_destroy_crtc(config, crtc_cfg);
 
 	kfree(config->dev_name);
 	kfree(config);
@@ -121,9 +130,25 @@ static bool valid_plane_type(struct vkms_config *config)
 	return true;
 }
 
+static bool valid_crtc_number(struct vkms_config *config)
+{
+	size_t n_crtcs;
+
+	n_crtcs = list_count_nodes(&config->crtcs);
+	if (n_crtcs <= 0 || n_crtcs >= 32) {
+		pr_err("The number of CRTCs must be between 1 and 31");
+		return false;
+	}
+
+	return true;
+}
+
 bool vkms_config_is_valid(struct vkms_config *config)
 {
 	if (!valid_plane_number(config))
+		return false;
+
+	if (!valid_crtc_number(config))
 		return false;
 
 	if (!valid_plane_type(config))
@@ -138,13 +163,18 @@ static int vkms_config_show(struct seq_file *m, void *data)
 	struct drm_device *dev = entry->dev;
 	struct vkms_device *vkmsdev = drm_device_to_vkms_device(dev);
 	struct vkms_config_plane *plane_cfg;
+	struct vkms_config_crtc *crtc_cfg;
 
 	seq_printf(m, "dev_name=%s\n", vkmsdev->config->dev_name);
-	seq_printf(m, "writeback=%d\n", vkmsdev->config->writeback);
 
 	list_for_each_entry(plane_cfg, &vkmsdev->config->planes, link) {
 		seq_puts(m, "plane:\n");
 		seq_printf(m, "\ttype=%d\n", plane_cfg->type);
+	}
+
+	list_for_each_entry(crtc_cfg, &vkmsdev->config->crtcs, link) {
+		seq_puts(m, "crtc:\n");
+		seq_printf(m, "\twriteback=%d\n", crtc_cfg->writeback);
 	}
 
 	return 0;
@@ -179,4 +209,26 @@ void vkms_config_destroy_plane(struct vkms_config_plane *plane_cfg)
 {
 	list_del(&plane_cfg->link);
 	kfree(plane_cfg);
+}
+
+struct vkms_config_crtc *vkms_config_add_crtc(struct vkms_config *config)
+{
+	struct vkms_config_crtc *crtc_cfg;
+
+	crtc_cfg = kzalloc(sizeof(*crtc_cfg), GFP_KERNEL);
+	if (!crtc_cfg)
+		return ERR_PTR(-ENOMEM);
+
+	crtc_cfg->writeback = false;
+
+	list_add_tail(&crtc_cfg->link, &config->crtcs);
+
+	return crtc_cfg;
+}
+
+void vkms_config_destroy_crtc(struct vkms_config *config,
+			      struct vkms_config_crtc *crtc_cfg)
+{
+	list_del(&crtc_cfg->link);
+	kfree(crtc_cfg);
 }
